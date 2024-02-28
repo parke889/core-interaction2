4815a30387447e3d4346071015855c80d43b3038
parke889 <parke889@newschool.edu>
2024-02-28 09:01:22 -0500

    3

---
+++ p1/script.js
@@ -0,0 +1,7 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   var title = document.querySelector('.title');
+
+//   document.addEventListener("click", function() {
+//     title.style.display = "none";
+//   });
+// });
