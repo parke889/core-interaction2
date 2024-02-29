e881471e4a3f8e1353fb38d2b7e8696a6e368b1f
parke889 <parke889@newschool.edu>
2024-02-29 05:10:13 -0500

    1

--- p1/script.js
+++ p1/script.js
@@ -1,7 +1,14 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   var title = document.querySelector('.title');
+document.addEventListener("DOMContentLoaded", function() {
+  var title = document.querySelector('.title');
+  var two = document.querySelector('.praise');
+  var three = document.querySelector('.shadows');
+  var intro = document.querySelector('.intro');
 
-//   document.addEventListener("click", function() {
-//     title.style.display = "none";
-//   });
-// });
+  document.addEventListener("click", function() {
+    title.style.display = "none";
+    two.style.display = "none";
+    three.style.display = "none";
+
+    intro.style.display = "block"
+  });
+});
