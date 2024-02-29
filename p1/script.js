29f480a66ad36377b64cf078fdece9ad25c27e06
parke889 <parke889@newschool.edu>
2024-02-29 13:57:18 -0500

    4

--- p1/script.js
+++ p1/script.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
   var title = document.querySelector('.title');
   var two = document.querySelector('.praise');
   var three = document.querySelector('.shadows');
-  var intro = document.querySelector('.intro');
 
   document.addEventListener("click", function() {
     title.style.display = "none";
     two.style.display = "none";
     three.style.display = "none";
 
-    intro.style.display = "block"
   });
 });
