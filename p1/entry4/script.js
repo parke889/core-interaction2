4b97c347f57b618402456d5c7eaca308d39b16cf
parke889 <parke889@newschool.edu>
2024-03-01 08:25:41 -0500

    3

--- p1/entry4/script.js
+++ p1/entry4/script.js
@@ -13,3 +13,12 @@ for (var i = 0; i < 10; i++) { // Adjust the number of spans as needed
     span.style.top = position.y + "px";
     bubbleDiv.appendChild(span);
 }
+
+// $(document).ready(function($) {
+//     $(".bubble").click(function(event){
+//         console.log(".bubble");         
+//         event.preventDefault();
+//         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+//     });
+
+// });
